dd821723ffd3bc92aef8c68f964ca06bfa4899c5
ssharma0262-del <ssharma0262@gmail.com>
2026-08-22 17:27:27 +0530

    about variables and datatypes type function operators and input function

---
+++ CH2- variables and datatypes/02_datatpes.py
@@ -0,0 +1,5 @@
+a=1 # a is an integer
+b=5.22 # b is a floating integer
+c="aniket" # c is a string
+d= False # d is a boolean variable
+e=None # e is none type variable
---
+++ CH2- variables and datatypes/03_rules_variables.py
@@ -0,0 +1,13 @@
+a=23
+
+aaa=435
+
+Aniket=34
+
+sunny=45
+
+
+_sunny =34
+
+# @sunny = 54 invalid due to @symbol
+# s@unny invalid due to @ symbol
