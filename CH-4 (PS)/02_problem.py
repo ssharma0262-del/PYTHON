eb5437d06c08d0eb70e8b267ed3514a6c2c87fdb
ssharma0262-del <ssharma0262@gmail.com>
2026-08-26 20:30:54 +0530

    problems on list and tuples

---
+++ CH-4 (PS)/02_problem.py
@@ -0,0 +1,16 @@
+Marks=[]
+f1=int(input(("enter Marks here:")))
+Marks.append(f1)
+f2=int(input(("enter Marks here:")))
+Marks.append(f2)
+f3=int(input(("enter Marks here:")))
+Marks.append(f3)
+f4=int(input(("enter Marks here:")))
+Marks.append(f4)
+f5=int(input(("enter Marks here:")))
+Marks.append(f5)
+f6=int(input(("enter Marks here:")))
+Marks.append(f6)
+Marks.sort()
+print(Marks)
+
