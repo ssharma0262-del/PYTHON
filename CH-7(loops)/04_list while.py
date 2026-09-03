e154ce55f44e853e5ff5dbb144191a576b3d4d81
ssharma0262-del <ssharma0262@gmail.com>
2026-09-03 10:34:43 +0530

    about loops

---
+++ CH-7(loops)/04_list while.py
@@ -0,0 +1,5 @@
+l=[1,"aniket","false","This","Piyush","Aryan"]
+i=0
+while(i<len(l)):
+    print(l[i])
+    i=i+1
---
+++ CH-7(loops)/08_break and continue.py
@@ -0,0 +1,15 @@
+for i in range(100):
+    if(i==34):
+        break
+    print(i)
+
+for i in range(100):
+    if(i==34):
+        continue
+    print(i)
+
+for i in range(4):
+    print("printing")
+    if i==2:
+        continue
+    print(i)
